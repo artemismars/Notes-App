20d1bdb6767f274406626686c3ad352d3b270e32
artemismars <artemismars@outlook.com>
2022-03-26 18:06:51 +0900

    Update CORS

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,26 @@ router.post("/notes", async (req, res) => {
   try {
     const note = await db.Note.create(req.body);
     console.log(note);
+
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
     res.status(202).json(note);
   } catch (error) {
     res.status(400).json({ message: error.message });
